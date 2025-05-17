573e88233aedd5a0a97c4b40a3861fae8fd4370a
Saksham Consul <saksham@blackforestlabs.ai>
2025-05-16 17:05:31 -0700

    Fix expand unit test

--- src/blackforest/resources/mapping/model_input_registry.py
+++ src/blackforest/resources/mapping/model_input_registry.py
@@ -2,6 +2,7 @@ from blackforest.types.inputs.flux_dev import FluxDevInputs
 from blackforest.types.inputs.flux_pro import FluxProInputs
 from blackforest.types.inputs.flux_pro_1_1 import FluxPro11Inputs
 from blackforest.types.inputs.flux_pro_canny import FluxProCannyInputs
+from blackforest.types.inputs.flux_pro_depth import FluxProDepthInputs
 from blackforest.types.inputs.flux_pro_expand import FluxProExpandInputs
 from blackforest.types.inputs.flux_pro_fill import FluxProFillInputs
 from blackforest.types.inputs.flux_ultra import FluxUltraInputs
@@ -14,4 +15,5 @@ MODEL_INPUT_REGISTRY = {
     "flux-pro-1.0-fill": FluxProFillInputs,
     "flux-pro-1.0-expand": FluxProExpandInputs,
     "flux-pro-1.0-canny": FluxProCannyInputs,
+    "flux-pro-1.0-depth": FluxProDepthInputs,
 }
